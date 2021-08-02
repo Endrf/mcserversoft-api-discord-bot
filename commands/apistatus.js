ce7570832281992387eec569dcd4effe114eef66
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 05:22:53 -0400

    updated with new things

--- commands/apistatus.js
+++ commands/apistatus.js
@@ -1,26 +1,32 @@
 const Discord = require('discord.js');
 const fse = require('fs-extra');
 const fetch = require('node-fetch');
-const client = new Discord.Client();
 const config = require('../config.json');
 
-exports.run = async (message) => {
-    try {
-        apiMessage = await fetch(config.mcssURL + ":" + config.mcssPort)
-        .then(response => response.json());
-    } catch(error) {
-        let embed = new Discord.MessageEmbed()
-        .setColor("#e63939")
-        .setTitle("Api Status:")
-        .setDescription("```css\n[OFFLINE]```\nReason: " + error.name);
-        message.channel.send(embed);
-        return;
-    }
-    if (apiMessage.message) {
-        let embed = new Discord.MessageEmbed()
-        .setColor(config.embedColor)
-        .setTitle("Api Status:")
-        .setDescription("```ini\n[ONLINE]```");
-        message.channel.send(embed);
-    }
+module.exports = {
+    "name":"apistatus",
+    "enabled":true,
+    "description":"idek man, i think it gets the api status",
+    "help":"there are like things here, idk, that mean some status things",
+    async run (message, client) {
+        try {
+            apiMessage = await fetch(`${config.mcssURL}:${config.mcssPort}`)
+            .then(response => response.json());
+        } catch(error) {
+            let embed = new Discord.MessageEmbed()
+                .setColor("#e63939")
+                .setTitle("Api Status:")
+                .setDescription("```css\n[OFFLINE]```\nReason: " + error.name);
+            message.channel.send(embed);
+            return;
+        }
+
+        if (apiMessage.message) {
+            let embed = new Discord.MessageEmbed()
+                .setColor(config.embedColor)
+                .setTitle("Api Status:")
+                .setDescription("```ini\n[ONLINE]```");
+            message.channel.send(embed);
+        }
+    }    
 }
