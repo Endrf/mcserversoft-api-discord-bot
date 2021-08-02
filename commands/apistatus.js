9fa87b7f1db812bf8200011e6d49640dad69831c
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 02:10:17 -0400

    slight reformatting
    removed invorrect and obsolete if statement
    fixed indentation
    added ;

--- commands/apistatus.js
+++ commands/apistatus.js
@@ -6,23 +6,21 @@ const config = require('../config.json');
 
 exports.run = async (message) => {
     try {
-    apiMessage = await fetch(config.mcssURL + ":" + config.mcssPort)
-    .then(response => response.json())
+        apiMessage = await fetch(config.mcssURL + ":" + config.mcssPort)
+        .then(response => response.json());
     } catch(error) {
         let embed = new Discord.MessageEmbed()
         .setColor("#e63939")
         .setTitle("Api Status:")
         .setDescription("```css\n[OFFLINE]```\nReason: " + error.name)
-        message.channel.send(embed)
-        if(error = true) {
-            return;
-        }
+        message.channel.send(embed);
+        return;
     }
     if (apiMessage.message) {
-    let embed = new Discord.MessageEmbed()
-    .setColor(config.embedColor)
-    .setTitle("Api Status:")
-    .setDescription("```ini\n[ONLINE]```")
-    message.channel.send(embed)
+        let embed = new Discord.MessageEmbed()
+        .setColor(config.embedColor)
+        .setTitle("Api Status:")
+        .setDescription("```ini\n[ONLINE]```");
+        message.channel.send(embed);
     }
 }
