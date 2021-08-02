fdad2d59651a98cb3ca51ef118e0094bdcaf9136
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 06:11:44 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,24 @@
 const Discord = require('discord.js');
 const fse = require('fs-extra');
 const fetch = require('node-fetch');
-const client = new Discord.Client();
 const config = require('../config.json');
 
 module.exports = {
     "help":"Displays the bots commands.",
-    "apistatus":"Displays the connection to the api.",
-    "version":"Displays the version of mcss, api, and whether it's a dev build or not.",
-    "servers":"Displays all the servers you have on mcss.",
-    "serversfull":"Displays more information on all the servers you have on mcss.",
-    async run(message) {
+    //"apistatus":"Displays the connection to the api.",
+    //"version":"Displays the version of mcss, api, and whether it's a dev build or not.",
+    //"servers":"Displays all the servers you have on mcss.",
+    //"serversfull":"Displays more information on all the servers you have on mcss.",
+    async run(message, client) {
         let embed = new Discord.MessageEmbed()
-        .setColor(config.embedColor)
-        .setTitle("Commands:")
-        .setDescription("Be sure to add " + config.prefix + " before the command" + 
-        "\n**`help`** - " + this.help + 
-        "\n**`apistatus`** - " + this.apistatus + 
-        "\n**`version`** - " + this.version + 
-        "\n**`servers`** - " + this.servers + 
-        "\n**`serversfull`** - " + this.serversfull)
+            .setColor(config.embedColor)
+            .setTitle("Commands:")
+            .setDescription("Be sure to add `" + config.prefix + "` before the command"
+        
+        for(command of client.commands) {
+            if(command.enabled) embed.addField(command.name, command.help);
+        }
+
         message.channel.send(embed)
     }
 }
