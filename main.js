f3148165695f0a76a7e613fd90bf1d39fd1a45a6
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 06:02:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const fetch = require('node-fetch');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync("./commands/")
+    .filter(file => file.endsWith(".js"))
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`)
+    client.commands.set(command.name, command)
+}
+
 client.on('ready', () => {
     var username = client.user.username
     console.log(`${username} is Online`);
@@ -15,9 +24,8 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content.startsWith(config.prefix)) {
         let cmd = message.content.toLowerCase().slice(config.prefix.length)
-        if (fse.existsSync(`./commands/${cmd}.js`)){
-        let file = require(`./commands/${cmd}.js`);
-        file.run(message)
+        client.commands.get(cmd).run(message, client) // probably will cause errors when sending invalid commands
+        //file.run(message, client)
         }
     }
 })
