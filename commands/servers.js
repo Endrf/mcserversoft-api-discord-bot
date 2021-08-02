74e0dcd2e5a600fb0f25404f64da753d7e58c006
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 05:33:09 -0400

    updated rhe fhhff

--- commands/servers.js
+++ commands/servers.js
@@ -1,35 +1,39 @@
 const Discord = require('discord.js');
 const fse = require('fs-extra');
 const fetch = require('node-fetch');
-const client = new Discord.Client();
 const config = require('../config.json');
 
-exports.run = async (message) => {
-    //API Token Request
-    const params = new URLSearchParams();
-    params.append('username', config.mcssUsername);
-    params.append('password', config.mcssPassword);
+module.exports = {
+    "name":"servers",
+    "enabled":true,
+    "description":"idek man, i think it shos servers",
+    "help":"there are like things here, idk, that mean some status things",
+    async run (message, client) {
+        //API Token Request
+        const params = new URLSearchParams();
+        params.append('username', config.mcssUsername);
+        params.append('password', config.mcssPassword);
     
-    try {
-        apiToken = await fetch(`${config.mcssURL}:${config.mcssPort}/api/token`, { method: 'POST', body: params })
-        .then(response => response.json());
-    } catch(error) {
-        let embed = new Discord.MessageEmbed()
-            .setColor("#e63939")
-            .setTitle("ERROR:")
-            .setDescription(`Reason: ${error.name}`)
-        message.channel.send(embed)
-        return;
-    };
+        try {
+            apiToken = await fetch(`${config.mcssURL}:${config.mcssPort}/api/token`, { method: 'POST', body: params })
+            .then(response => response.json());
+        } catch(error) {
+            let embed = new Discord.MessageEmbed()
+                .setColor("#e63939")
+                .setTitle("ERROR:")
+                .setDescription(`Reason: ${error.name}`)
+            message.channel.send(embed);
+            return;
+        }
 
-    //API Servers Request
-    const headers = {
-        `Authorization`: `Bearer ${apiToken.access_token}`
-    };
+        //API Servers Request
+        const headers = {
+            `Authorization`: `Bearer ${apiToken.access_token}`
+        }
 
-    try {
-        apiMessage = await fetch(`${config.mcssURL}:${config.mcssPort}/api/servers/minimal`, { headers: headers })
-        .then(response => response.json());
+        try {
+            apiMessage = await fetch(`${config.mcssURL}:${config.mcssPort}/api/servers/minimal`, { headers: headers })
+            .then(response => response.json());
         } catch(error) {
             let embed = new Discord.MessageEmbed()
                 .setColor("#e63939")
@@ -37,7 +41,7 @@ exports.run = async (message) => {
                 .setDescription(`Reason: ${error.name}`);
             message.channel.send(embed);
             return;
-        };
+        }
 
         let embed = new Discord.MessageEmbed()
             .setColor(config.embedColor)
@@ -45,9 +49,9 @@ exports.run = async (message) => {
 
         for(server in apiMessage) {
             statusTypes = [
-                "css\n[OFFLINE]",
+            "css\n[OFFLINE]",
                 "ini\n[ONLINE]",
-                "json\n[New classified feature that doesn't exist idk]"
+                "css\n[NONE]"
                 "fix\n[STARTING]",
                 "css\n[STOPPING]"
             ];
@@ -57,6 +61,7 @@ exports.run = async (message) => {
             embed.addField(`**Server ${server + 1}**`, "**Server Name: **```" + apiMessage[loop].Name + "```" + 
             "\n**Server Status: **" + status + 
             "\n**Server ID: **```" + apiMessage[loop].Guid + "```", true);
-        };
-        message.channel.send(embed)
+        }
+        message.channel.send(embed);
+    }
 }
