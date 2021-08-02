14acefa791b3521edc2998371023966644d6725a
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 03:00:12 -0400

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -9,57 +9,53 @@ exports.run = async (message) => {
     const params = new URLSearchParams();
     params.append('username', config.mcssUsername);
     params.append('password', config.mcssPassword);
+    
     try {
-    apiToken = await fetch(config.mcssURL + ":" + config.mcssPort + "/api/token", { method: 'POST', body: params })
-    .then(response => response.json());
+        apiToken = await fetch(`${config.mcssURL}:${config.mcssPort}/api/token`, { method: 'POST', body: params })
+        .then(response => response.json());
     } catch(error) {
         let embed = new Discord.MessageEmbed()
             .setColor("#e63939")
             .setTitle("ERROR:")
-            .setDescription("Reason: " + error.name)
-            message.channel.send(embed)
-        if(error = true) {
-            return;
-        }
-    }
+            .setDescription(`Reason: ${error.name}`)
+        message.channel.send(embed)
+        return;
+    };
 
     //API Servers Request
     const headers = {
-        'Authorization': 'Bearer ' + apiToken.access_token
-    }
+        `Authorization`: `Bearer ${apiToken.access_token}`
+    };
+
     try {
-        apiMessage = await fetch(config.mcssURL + ":" + config.mcssPort + "/api/servers/minimal", { headers: headers })
-        .then(response => response.json())
+        apiMessage = await fetch(`${config.mcssURL}:${config.mcssPort}/api/servers/minimal`, { headers: headers })
+        .then(response => response.json());
         } catch(error) {
             let embed = new Discord.MessageEmbed()
-            .setColor("#e63939")
-            .setTitle("ERROR:")
-            .setDescription("Reason: " + error.name)
-            message.channel.send(embed)
-            if(error = true) {
-                return;
-            }
-        }
-        loop = 0
-        server = 1
+                .setColor("#e63939")
+                .setTitle("ERROR:")
+                .setDescription(`Reason: ${error.name}`);
+            message.channel.send(embed);
+            return;
+        };
+
         let embed = new Discord.MessageEmbed()
-        .setColor(config.embedColor)
-        .setTitle("Servers:")
-        while (loop < apiMessage.length) {
-            if (apiMessage[loop].Status == 0) {
-                status = "```css\n[OFFLINE]```"
-            } else if (apiMessage[loop].Status == 1) {
-                status = "```ini\n[ONLINE]```"
-            } else if (apiMessage[loop].Status == 3) {
-                status = "```fix\n[STARTING]```"
-            } else if (apiMessage[loop].Status == 4) {
-                status = "```css\n[STOPPING]```"
-            }
-            embed.addField("**Server " + server + "**", "**Server Name: **```" + apiMessage[loop].Name + "```" + 
+            .setColor(config.embedColor)
+            .setTitle("Servers:")
+
+        for(server in apiMessage) {
+            statusTypes = [
+                "css\n[OFFLINE]",
+                "ini\n[ONLINE]",
+                "fix\n[STARTING]",
+                "css\n[STOPPING]"
+            ];
+
+            status = "```" + statusTypes[server] + "```";
+
+            embed.addField(`**Server ${server + 1}**`, "**Server Name: **```" + apiMessage[loop].Name + "```" + 
             "\n**Server Status: **" + status + 
             "\n**Server ID: **```" + apiMessage[loop].Guid + "```", true);
-            loop = loop + 1
-            server = server + 1
-        }
+        };
         message.channel.send(embed)
 }
