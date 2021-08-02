cad205f7a900dbd0ce2c7a2b146520d04e6fd6ad
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 05:54:10 -0400

    removed new client

--- commands/version.js
+++ commands/version.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const fse = require('fs-extra');
 const fetch = require('node-fetch');
-const client = new Discord.Client();
 const config = require('../config.json');
 
 module.exports = {
