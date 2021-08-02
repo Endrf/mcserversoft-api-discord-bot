7df5f008cff4360e12cce9280696940cf8d9f999
PhilipFV <70909605+PhilipFV@users.noreply.github.com>
2021-08-02 05:44:07 -0400

    djjdididkdjfjdjdjdjdjjdjfxudidudududududududud

--- commands/serversfull.js
+++ commands/serversfull.js
@@ -1,65 +1,66 @@
 const Discord = require('discord.js');
 const fse = require('fs-extra');
 const fetch = require('node-fetch');
-const client = new Discord.Client();
 const config = require('../config.json');
 
-exports.run = async (message) => {
-    //API Token Request
-    const params = new URLSearchParams();
-    params.append('username', config.mcssUsername);
-    params.append('password', config.mcssPassword);
-    try {
-    apiToken = await fetch(config.mcssURL + ":" + config.mcssPort + "/api/token", { method: 'POST', body: params })
-    .then(response => response.json());
-    } catch(error) {
-        let embed = new Discord.MessageEmbed()
-            .setColor("#e63939")
-            .setTitle("ERROR:")
-            .setDescription("Reason: " + error.name)
-            message.channel.send(embed)
-        if(error = true) {
-            return;
-        }
-    }
+module.exports = {
+    "name":"apistatus",
+    "enabled":true,
+    "description":"idek man, i think it gets the api status",
+    "help":"there are like things here, idk, that mean some status things",
+    async run (message, client) {
 
-    //API Servers Request
-    const headers = {
-        'Authorization': 'Bearer ' + apiToken.access_token
-    }
-    try {
-        apiMessage = await fetch(config.mcssURL + ":" + config.mcssPort + "/api/servers", { headers: headers })
-        .then(response => response.json())
+        //API Token Request
+        const params = new URLSearchParams();
+        params.append('username', config.mcssUsername);
+        params.append('password', config.mcssPassword);
+        try {
+            apiToken = await fetch(`${config.mcssURL}:${config.mcssPort}/api/token`, { method: 'POST', body: params })
+            .then(response => response.json());
         } catch(error) {
             let embed = new Discord.MessageEmbed()
-            .setColor("#e63939")
-            .setTitle("ERROR:")
-            .setDescription("Reason: " + error.name)
-            message.channel.send(embed)
+                .setColor("#e63939")
+                .setTitle("ERROR:")
+                .setDescription(`Reason: ${error.name}`);
+            message.channel.send(embed);
+            return;
         }
-        loop = 0
-        server = 1
+
+        //API Servers Request
+        const headers = {
+            `Authorization`: `Bearer ${apiToken.access_token}`
+        }
+        try {
+            apiMessage = await fetch(config.mcssURL + ":" + config.mcssPort + "/api/servers", { headers: headers })
+            .then(response => response.json())
+        } catch(error) {
+            let embed = new Discord.MessageEmbed()
+                .setColor("#e63939")
+                .setTitle("ERROR:")
+                .setDescription(`Reason: ${error.name}`);
+            message.channel.send(embed);
+            return;
+        }
+
         let embed = new Discord.MessageEmbed()
-        .setColor(config.embedColor)
-        .setTitle("Servers:")
-        while (loop < apiMessage.length) {
-            if (apiMessage[loop].Status == 0) {
-                status = "```css\n[OFFLINE]```"
-            } else if (apiMessage[loop].Status == 1) {
-                status = "```ini\n[ONLINE]```"
-            } else if (apiMessage[loop].Status == 3) {
-                status = "```fix\n[STARTING]```"
-            } else if (apiMessage[loop].Status == 4) {
-                status = "```css\n[STOPPING]```"
-            }
-            embed.addField("**Server " + server + "**", "**Server Name: **```" + apiMessage[loop].Name + "```" + 
+            .setColor(config.embedColor)
+            .setTitle("Servers:")
+
+        for(server in apiMessage) {
+            statusTypes = [
+                "css\n[OFFLINE]",
+                "ini\n[ONLINE]",
+                "css\n[NONE]",
+                "fix\n[STARTING]",
+                "css\n[STOPPING]" ];
+
+            embed.addField("**Server " + server + 1 + "**", "**Server Name: **```" + apiMessage[loop].Name + "```" + 
             "\n**Server Status: **" + status + 
             "\n**Server ID: **```" + apiMessage[loop].Guid + "```" + 
             "\n**Server Path: **```" + apiMessage[loop].PathToFolder + "```" + 
             "\n**Auto Start: **```" + apiMessage[loop].IsSetToAutoStart + "```" + 
             "\n**Keep Online Mode: **```" + apiMessage[loop].KeepOnline + "```",true);
-            loop = loop + 1
-            server = server + 1
         }
-        message.channel.send(embed)
+        message.channel.send(embed);
+    }
 }
